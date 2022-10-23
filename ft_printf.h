ffbd21502db5121630ffac70eda371d79a5371ee
busmanov <busmanov@student.42wolfsburg.de>
2022-10-23 19:07:00 +0200

    yeah

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: busmanov <busmanov@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 18:41:36 by busmanov          #+#    #+#             */
+/*   Updated: 2022/10/23 18:43:29 by busmanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include 
+
+int	ft_printf(const char *, ...);
+
+#endif
